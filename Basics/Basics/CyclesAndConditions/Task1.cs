eab562c48279bbc1ad3c7b883ce6e0b35efa4846
Nickolay <kolaivanov040@gmail.com>
2024-07-02 11:08:40 +0300

    task1 for new chapter

---
+++ Basics/Basics/CyclesAndConditions/Task1.cs
@@ -0,0 +1,16 @@
+﻿namespace Basics.CyclesAndConditions
+{
+    internal class Task1
+    {
+        public Task1()
+        {
+            Console.WriteLine("Введите вывод");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -1,32 +1,10 @@
-﻿using Basics.VariablesAndDataTypes;
-
-namespace VarVariablesAndDataTypes;
+﻿//using Basics.VariablesAndDataTypes;
+using Basics.CyclesAndConditions;
 
 class Program
 {
     static void Main()
     {
-        ////Задача #1////
-        //var task1 = new Task1();
-        ////Задача #1////
-
-        ////Задача #2////
-        ///int[] ar = { 1, 2, 3, 4 };
-        //var task2 = new Task2();
-        ////Задача #2////
-
-        ////Задача #3////
-        //var task3 = new Task3();
-        //task3.Start();
-        ////Задача #3////
-
-        ////Задача #4////
-        //var task4 = new Task4();
-        //task4.Start();
-        ////Задача #4////
-
-        ////Задача #5////
-        var task5 = new Task5();
-        ////Задача #5////
+        Task1 task1 = new Task1();
     }
 }
