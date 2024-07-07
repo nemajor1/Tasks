54e73b217e223b462d679a24052651501f725747
AR_ivanov <ivanov@endesign.ru>
2024-07-07 19:38:11 +0300

    2 task in 4 part

--- Basics/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ Basics/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01fcccd03ad94c77681ce28fc586c761462b43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451282ac913bc64da066d909df17519ab3ecfd6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
